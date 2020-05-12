f1cf89893251fad9603f13452faf200705ab34c8
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-05-12 15:43:14 -0700

    Trailing spaces

--- octoprint_thespaghettidetective/bin/janus/run_janus.sh
+++ octoprint_thespaghettidetective/bin/janus/run_janus.sh
@@ -2,7 +2,7 @@
 
 JANUS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-_term() { 
+_term() {
   kill -TERM "$child" 2>/dev/null
 }
 
@@ -10,5 +10,5 @@ trap _term SIGTERM
 
 LD_LIBRARY_PATH=$JANUS_DIR/lib:$JANUS_DIR/lib/janus:$LD_LIBRARY_PATH nice $JANUS_DIR/bin/janus -o --stun-server=stun.l.google.com:19302 --configs-folder=$JANUS_DIR/etc/janus &
 
-child=$! 
+child=$!
 wait "$child"
