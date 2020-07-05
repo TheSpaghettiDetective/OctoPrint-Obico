ac126a0388ecb7b9de19016dd7c4d8d104a9902f
Kenneth Jiang <kenneth.jiang@gmail.com>
2020-07-04 17:21:00 -0700

    Make package name to be compatible

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_thespaghettidetective"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Access Anywhere - The Spaghetti Detective"
+plugin_name = "TheSpaghettiDetective"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.2.5"
