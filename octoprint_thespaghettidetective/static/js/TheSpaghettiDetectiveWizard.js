a9232f3e5b9da9b268053c260783e30007b02fab
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-01-04 14:19:10 -0800

    Refactor to make the wizard more concise

--- octoprint_thespaghettidetective/static/js/TheSpaghettiDetectiveWizard.js
+++ octoprint_thespaghettidetective/static/js/TheSpaghettiDetectiveWizard.js
@@ -25,7 +25,8 @@ $(function () {
         // self.loginStateViewModel = parameters[0];
         self.settingsViewModel = parameters[0];
 
-        self.step = ko.observable(1);
+        self.step = ko.observable(2);
+        self.mobileFlow = ko.observable(true);
         self.securityCode = ko.observable('');
         self.verifying = ko.observable(false);
         self.userAgreementChecked = ko.observable(true);
