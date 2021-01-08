97305a1160f501032284d2c989611dd1673e12b3
Kenneth Jiang <kenneth.jiang@gmail.com>
2021-01-08 07:32:48 -0800

    Settings ViewModel

---
+++ octoprint_thespaghettidetective/static/js/TheSpaghettiDetectiveSettings.js
@@ -0,0 +1,40 @@
+/*
+ * View model for TheSpaghettiDetective Wizard
+ *
+ * Author: The Spaghetti Detective
+ * License: AGPLv3
+ */
+$(function () {
+
+    function apiCommand(data) {
+        return $.ajax("api/plugin/thespaghettidetective", {
+            method: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(data)
+        });
+    }
+
+    function TheSpaghettiDetectiveSettingsViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        self.settingsViewModel = parameters[0];
+
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: TheSpaghettiDetectiveSettingsViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: ["settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_thespaghettidetective, #tab_plugin_thespaghettidetective, ...
+        elements: [
+            "#settings_plugin_thespaghettidetective",
+        ]
+    });
+
+});
