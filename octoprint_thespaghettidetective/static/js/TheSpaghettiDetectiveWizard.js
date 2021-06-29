e87b542458afa49edc1c77bf50d118f19fb083eb
Dmitry Puzyrev <hello@dmitry.engineer>
2021-06-29 13:26:12 +0300

    Add 'tsd-' prefix to plugin classes

--- octoprint_thespaghettidetective/static/js/TheSpaghettiDetectiveWizard.js
+++ octoprint_thespaghettidetective/static/js/TheSpaghettiDetectiveWizard.js
@@ -33,7 +33,7 @@ $(function () {
 
         // Handle verification code typing:
 
-        $.find('.verification-code-input input').forEach(function(input) {
+        $.find('.tsd-verification-code-input input').forEach(function(input) {
             // Add event listener for paste event
             $(input).on('paste', handlePaste);
 
@@ -62,7 +62,7 @@ $(function () {
 
             if (event.key === 'Backspace') {
                 for (let i = 6; i >= 1; i--) {
-                    let input = $('.verification-code-input input[data-number='+ i +']');
+                    let input = $('.tsd-verification-code-input input[data-number='+ i +']');
                     if (input.val()) {
                         input.val('').trigger('focus');
                         self.securityCode(self.securityCode().slice(0, -1));
@@ -86,7 +86,7 @@ $(function () {
             }
 
             for (let i = 1; i <= 6; i++) {
-                let input = $('.verification-code-input input[data-number='+ i +']');
+                let input = $('.tsd-verification-code-input input[data-number='+ i +']');
 
                 if (!input.val()) {
                     // Put value to input visible to user
@@ -94,7 +94,7 @@ $(function () {
 
                     // Move cursor forward
                     if (i < 6) {
-                        $('.verification-code-input input[data-number='+ (i + 1) +']').trigger('focus');
+                        $('.tsd-verification-code-input input[data-number='+ (i + 1) +']').trigger('focus');
                     }
 
                     // Append char to inner code
@@ -114,7 +114,7 @@ $(function () {
 
         // Clear error messages from verification code
         function clearVerificationCodeMessages() {
-            $('.verification-wrapper').removeClass(['error', 'success', 'unknown']);
+            $('.tsd-verification-wrapper').removeClass(['error', 'success', 'unknown']);
         }
 
 
@@ -122,7 +122,7 @@ $(function () {
             self.toStep(self.step() + 1);
 
             if (self.step() === 4) {
-                $('.modal[aria-hidden="false"] .verification-code-input input[data-number=1]').trigger('focus');
+                $('.tsd-modal[aria-hidden="false"] .tsd-verification-code-input input[data-number=1]').trigger('focus');
             } else if (self.step() === 5) {
                 // Close button with countdown animation
                 setTimeout(function() {
@@ -157,39 +157,6 @@ $(function () {
             self.printerNameTimeoutId(newTimeoutId);
         })
 
-        self.savePrinterName = function() {
-            var newName = self.printerName().trim();
-            if (newName.length == 0) {
-                return;
-            }
-
-            // Saving in progress animation
-            $('.printerNameInput').addClass('saving-in-progress');
-            $('.printerNameInput .error-message').hide();
-
-            apiCommand({
-                command: "update_printer",
-                name: newName})
-                .done(function(result) {
-                    $('.printerNameInput').removeClass('saving-in-progress');
-
-                    if (result.succeeded) {
-                        $('.printerNameInput').addClass('successfully-saved');
-                        setTimeout(() => $('.printerNameInput').removeClass('successfully-saved'), 2000);
-                    } else {
-                        $('.printerNameInput').addClass('error-occurred');
-                        setTimeout(() => $('.printerNameInput').removeClass('error-occurred'), 2000);
-                        $('.printerNameInput .error-message').show();
-                    }
-                })
-                .fail(function() {
-                    $('.printerNameInput').removeClass('saving-in-progress');
-                    $('.printerNameInput').addClass('error-occurred');
-                    setTimeout(() => $('.printerNameInput').removeClass('error-occurred'), 2000);
-                    $('.printerNameInput .error-message').show();
-                });
-        }
-
         self.verifySecurityCode = function(code) {
             if (code.length !== 6) {
                 return;
@@ -203,18 +170,18 @@ $(function () {
             })
                 .done(function(apiStatus) {
                     if (apiStatus.succeeded == null) {
-                        $('.verification-wrapper').addClass('unknown');
+                        $('.tsd-verification-wrapper').addClass('unknown');
                     }
                     else if (apiStatus.succeeded) {
-                        $('.verification-wrapper').addClass('success');
+                        $('.tsd-verification-wrapper').addClass('success');
                         self.printerName(apiStatus.printer.name);
                         self.nextStep();
                     } else {
-                        $('.verification-wrapper').addClass('error');
+                        $('.tsd-verification-wrapper').addClass('error');
                     }
                 })
                 .fail(function() {
-                    $('.verification-wrapper').addClass('unknown');
+                    $('.tsd-verification-wrapper').addClass('unknown');
                 })
                 .always(function () {
                     self.verifying(false);
@@ -231,11 +198,11 @@ $(function () {
             self.securityCode('');
             $('.tsd-auto-close').removeClass('active');
 
-            let verificationWrapper = $('.verification-wrapper');
+            let verificationWrapper = $('.tsd-verification-wrapper');
             verificationWrapper.removeClass('success error unknown');
 
             for (let i = 1; i <= 6; i++) {
-                verificationWrapper.find('.verification-code-input input[data-number='+ i +']').val('');
+                verificationWrapper.find('.tsd-verification-code-input input[data-number='+ i +']').val('');
             }
         }
 
@@ -246,22 +213,22 @@ $(function () {
 
         // Next feature in the slider on home screen
         self.nextFeature = function() {
-            let container = $('.features').last();
-            let slidesCount = container.find('.feature').length;
+            let container = $('.tsd-features').last();
+            let slidesCount = container.find('.tsd-feature').length;
             let currentSlide = self.currentFeatureSlide();
             let nextSlide = currentSlide === slidesCount ? 1 : currentSlide + 1;
 
-            container.find('.feature[data-number="'+ currentSlide +'"]').animate({
+            container.find('.tsd-feature[data-number="'+ currentSlide +'"]').animate({
                 left: '-100%'
             }, {duration: 500, queue: false});
 
-            container.find('.feature[data-number="'+ nextSlide +'"]').animate({
+            container.find('.tsd-feature[data-number="'+ nextSlide +'"]').animate({
                 left: '0'
             },
             500,
             function() {
                 let next = nextSlide === slidesCount ? 1 : nextSlide + 1;
-                container.find('.feature[data-number="'+ next +'"]').css('left', '100%');
+                container.find('.tsd-feature[data-number="'+ next +'"]').css('left', '100%');
             });
 
             self.currentFeatureSlide(nextSlide);
