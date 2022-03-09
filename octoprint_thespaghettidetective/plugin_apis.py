a03c6cf200b4caf6139ea836d7de8f571eed5c7b
encetamasb <encetamasb@gmail.com>
2022-03-09 13:12:08 +0100

    Fixed verify command

--- octoprint_thespaghettidetective/plugin_apis.py
+++ octoprint_thespaghettidetective/plugin_apis.py
@@ -27,7 +27,7 @@ def verify_code(plugin, data):
         plugin._settings.set(["auth_token"], printer['auth_token'], force=True)
         plugin._settings.save(force=True)
         if configured_auth_token:
-            alert_queue.add_alert({'level': 'warning', 'cause': 'restart_required'}, self)
+            alert_queue.add_alert({'level': 'warning', 'cause': 'restart_required'}, plugin)
 
     return {'succeeded': succeeded, 'printer': printer}
 
