10ca7760c1fa2946d50d2190b1824a1e69706504
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-05-09 16:25:59 -0700

    Version need to be higher than the old repo since we are renaming the repo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_obico"
 plugin_name = "OctoPrint-Obico"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.0.1"
+plugin_version = "2.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
