a4d76aac6b7c29e2d2c759b026c399d61c74f6cb
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-08-21 18:43:16 -0700

    Messaging with server now is decoupled from octoprint

--- octoprint_obico/print_event.py
+++ octoprint_obico/print_event.py
@@ -20,8 +20,8 @@ class PrintEventTracker:
                 self.current_print_ts = int(time.time())
                 self._file_metadata_cache = None
 
-        data = self.octoprint_data(plugin)
-        data['octoprint_event'] = {
+        data = self.status(plugin)
+        data['event'] = {
             'event_type': event,
             'data': payload
         }
@@ -35,9 +35,9 @@ class PrintEventTracker:
 
         return data
 
-    def octoprint_data(self, plugin, status_only=False):
+    def status(self, plugin, status_only=False):
         data = {
-            'octoprint_data': plugin._printer.get_current_data()
+            'status': plugin._printer.get_current_data()
         }
 
         with self._mutex:
@@ -45,19 +45,19 @@ class PrintEventTracker:
             if self.tsd_gcode_file_id:
                 data['tsd_gcode_file_id'] = self.tsd_gcode_file_id
 
-        data['octoprint_data']['temperatures'] = plugin._printer.get_current_temperatures()
-        data['octoprint_data']['_ts'] = int(time.time())
+        data['status']['temperatures'] = plugin._printer.get_current_temperatures()
+        data['status']['_ts'] = int(time.time())
 
         if status_only:
             if self._file_metadata_cache:
-                data['octoprint_data']['file_metadata'] = self._file_metadata_cache
+                data['status']['file_metadata'] = self._file_metadata_cache
             return data
 
-        data['octoprint_data']['file_metadata'] = self._file_metadata_cache = self.get_file_metadata(plugin, data)
+        data['status']['file_metadata'] = self._file_metadata_cache = self.get_file_metadata(plugin, data)
 
         octo_settings = plugin.octoprint_settings_updater.as_dict()
         if octo_settings:
-            data['octoprint_settings'] = octo_settings
+            data['settings'] = octo_settings
 
         return data
 
@@ -71,7 +71,7 @@ class PrintEventTracker:
 
     def get_file_metadata(self, plugin, data):
         try:
-            current_file = data.get('octoprint_data', {}).get('job', {}).get('file', {})
+            current_file = data.get('status', {}).get('job', {}).get('file', {})
             origin = current_file.get('origin')
             path = current_file.get('path')
             if not origin or not path:
