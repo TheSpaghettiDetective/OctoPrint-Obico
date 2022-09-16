b92137a6f893fec72bd8cccfdb3e72e6adb7f113
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-09-16 10:14:16 -0700

    Prevent repeated events from being sent to the server

--- octoprint_obico/lib/alert_queue.py
+++ octoprint_obico/lib/alert_queue.py
@@ -7,6 +7,10 @@ from collections import deque
 
 ring_buffer = deque(maxlen=5)
 
+# We dont' want to bombard the server with repeated events. So we keep track of the events sent since last restart.
+# However, there are probably situations in the future repeated events do need to be propagated to the server.
+printer_events_posted = deque(maxlen=20)
+
 def add_alert(alert, plugin, post_to_server=False, attach_snapshot=False):
     global ring_buffer
     if alert in ring_buffer:
@@ -14,9 +18,11 @@ def add_alert(alert, plugin, post_to_server=False, attach_snapshot=False):
     ring_buffer.append(alert)
     plugin._plugin_manager.send_plugin_message(plugin._identifier, {'plugin_updated': True})
 
-    if post_to_server:
+    alert_title = alert.get('title')
+    if post_to_server and alert_title and not alert_title in printer_events_posted:
+        printer_events_posted.append(alert_title)
         plugin.post_printer_event_to_server(
-            alert['title'],
+            alert_title,
             alert['text'],
             event_class=('WARNING' if alert['level'] == 'warning' else 'ERROR'),
             attach_snapshot=attach_snapshot,
