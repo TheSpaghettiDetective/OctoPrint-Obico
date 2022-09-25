f045bbefe682940552b382d91d0e99134ee3513e
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-09-25 13:06:28 -0700

    Refactor to make it easier to send printer_event as passthru to client

--- octoprint_obico/lib/alert_queue.py
+++ octoprint_obico/lib/alert_queue.py
@@ -7,10 +7,6 @@ from collections import deque
 
 ring_buffer = deque(maxlen=5)
 
-# We dont' want to bombard the server with repeated events. So we keep track of the events sent since last restart.
-# However, there are probably situations in the future repeated events do need to be propagated to the server.
-printer_events_posted = deque(maxlen=20)
-
 def add_alert(alert, plugin, post_to_server=False, attach_snapshot=False):
     global ring_buffer
     if alert in ring_buffer:
@@ -19,15 +15,15 @@ def add_alert(alert, plugin, post_to_server=False, attach_snapshot=False):
     plugin._plugin_manager.send_plugin_message(plugin._identifier, {'plugin_updated': True})
 
     alert_title = alert.get('title')
-    if post_to_server and alert_title and not alert_title in printer_events_posted:
-        printer_events_posted.append(alert_title)
-        plugin.post_printer_event_to_server(
-            'Obico Plugin: ' + alert_title,
-            '<p><i>OctoPrint plugin error:</i></p><div>' + alert['text'] + '</div>',
-            event_class=('WARNING' if alert['level'] == 'warning' else 'ERROR'),
-            attach_snapshot=attach_snapshot,
-            info_url=alert.get('info_url', None)
+    if post_to_server and alert_title:
+        event_data = dict(
+            event_title = 'Obico Plugin: ' + alert_title,
+            event_text = '<p><i>OctoPrint plugin error:</i></p><div>' + alert['text'] + '</div>',
+            event_class = ('WARNING' if alert['level'] == 'warning' else 'ERROR'),
+            event_type = 'PRINTER_ERROR',
+            info_url = alert.get('info_url', None),
         )
+        plugin.post_printer_event_to_server(event_data=event_data,attach_snapshot=attach_snapshot)
 
 
 def fetch_and_clear():
