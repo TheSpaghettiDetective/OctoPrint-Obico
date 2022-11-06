c7dd7a50bd12b30d1924d9c508f912bce0888b8e
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-11-06 09:32:24 -0800

    Follow sentry's convention of semantic versioning

--- octoprint_obico/utils.py
+++ octoprint_obico/utils.py
@@ -121,12 +121,7 @@ class SentryWrapper:
             # django.contrib.auth) you may enable sending PII data.
             send_default_pii=True,
 
-            # By default the SDK will try to use the SENTRY_RELEASE
-            # environment variable, or infer a git commit
-            # SHA as release, however you may want to set
-            # something more human-readable.
-            # release="myapp@1.0.0",
-            release=plugin._plugin_version,
+            release='octoprint-obico@'+plugin._plugin_version,
         )
 
     def enabled(self):
