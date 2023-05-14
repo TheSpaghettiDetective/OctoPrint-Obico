d2238a15cf60f094eb62baebd285f9d98d60f480
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-05-14 07:49:13 -0700

    +global_header doesn't work with m2m encoder. I have learned it in moonraker-obico but have to relearn it here

--- octoprint_obico/webcam_stream.py
+++ octoprint_obico/webcam_stream.py
@@ -268,7 +268,11 @@ class WebcamStreamer:
                 _logger.debug('Popen: {}'.format(ffmpeg_cmd))
                 ffmpeg_test_proc = psutil.Popen(ffmpeg_cmd.split(' '), stdout=FNULL, stderr=FNULL)
                 if ffmpeg_test_proc.wait() == 0:
-                    return encoder
+                    if encoder == 'h264_omx':
+                        return '-flags:v +global_header -c:v {}'.format(encoder)  # Apparently OMX encoder needs extra param to get the stream to work
+                    else:
+                        return '-c:v {}'.format(encoder)
+
             raise Exception('No ffmpeg found, or ffmpeg does NOT support h264_omx/h264_v4l2m2m encoding.')
 
         wait_for_port_to_close('127.0.0.1', 8080)  # wait for WebcamServer to be clear of port 8080
@@ -296,7 +300,7 @@ class WebcamStreamer:
             fps = 5
             bitrate = int(bitrate/4)
 
-        self.start_ffmpeg('-re -i {} -filter:v fps={} -b:v {} -pix_fmt yuv420p -s {}x{} -flags:v +global_header -vcodec {} -bsf dump_extra'.format(stream_url, fps, bitrate, img_w, img_h, encoder))
+        self.start_ffmpeg('-re -i {} -filter:v fps={} -b:v {} -pix_fmt yuv420p -s {}x{} {}'.format(stream_url, fps, bitrate, img_w, img_h, encoder))
         self.compat_streaming = True
 
     def start_ffmpeg(self, ffmpeg_args):
