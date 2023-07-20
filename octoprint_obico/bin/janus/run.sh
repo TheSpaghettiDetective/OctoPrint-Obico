854498b19b792adf21fae5c2068c08239d4bcb2e
Your Name <you@example.com>
2023-07-20 21:42:51 +0100

    Revert "Use system janus as priority to see if it fixes the seg-fault problem in precomplied version"
    This reverts commit 3430ce5fbc45f528c5e9fbc5bcbe489db40c9a55.

--- octoprint_obico/bin/janus/run.sh
+++ octoprint_obico/bin/janus/run.sh
@@ -9,15 +9,13 @@ RUNTIME_JANUS_ETC_DIR="${JANUS_ROOT_DIR}/runtime/etc/janus"
 
 PRECOMPILED_DIR="${JANUS_ROOT_DIR}/precomplied/debian.$( debian_variant )"
 
-if which janus; then # janus is installed as systemm package
-  LIB_PATH="${LD_LIBRARY_PATH}"
-  JANUS_CMD="janus"
-elif is_raspberry_pi && [ -d "${PRECOMPILED_DIR}" ]; then
+if is_raspberry_pi && [ -d "${PRECOMPILED_DIR}" ]; then
   lib_janus_dir="${PRECOMPILED_DIR}/lib/janus"
   LIB_PATH="${PRECOMPILED_DIR}/lib:${LD_LIBRARY_PATH}"
   JANUS_CMD="${PRECOMPILED_DIR}/bin/janus"
 else
-  exit 1
+  LIB_PATH="${LD_LIBRARY_PATH}"
+  JANUS_CMD="janus"
 fi
 
 _term() {
