94b2b4e724aea3a0bd8bad4bdb0d94319708c294
aarontuftss <aarontufts.dev@gmail.com>
2023-08-16 17:20:22 -0600

    pr clean up & mark TODOs

--- octoprint_obico/celestrius.py
+++ octoprint_obico/celestrius.py
@@ -1,9 +1,7 @@
 import logging
 import time
 from octoprint_obico.utils import server_request
-
 from octoprint_obico.webcam_capture import capture_jpeg
-
 _logger = logging.getLogger('obico.celestrius')
 
 class Celestrius:
@@ -17,15 +15,15 @@ class Celestrius:
         while True:
             if self.on_first_layer == True:
                 try:
+                    #TODO replace webcam config with nozzle cam config
                     self.send_celestrius_jpeg(capture_jpeg(self.plugin)) #TODO replace argument with nozzle cam config
                     _logger.debug('Celestrius Jpeg captured & sent')
                 except Exception as e:
                     _logger.warning('Failed to capture jpeg - ' + str(e))
             time.sleep(0.2) #TODO how many photos do we want?
 
-
     def send_celestrius_jpeg(self, snapshot):
-        if snapshot: #TODO update with new endpoint & 
+        if snapshot: #TODO update with new endpoint & data
             try:
                 files = {'pic': snapshot}
                 server_request('POST', '/api/v1/octo/printer_events/', self.plugin, timeout=60, raise_exception=True, files=files, data=None, headers=self.plugin.auth_headers())
@@ -34,9 +32,9 @@ class Celestrius:
 
     def notify_server_celestrius_complete(self):
         self.on_first_layer = False
-        try:
-            data = {'celestrius_status': 'complete' }
-            #TODO update with new endpoint & data
+        try: #TODO update with new endpoint & data
+            data = {'celestrius_status': 'complete'}
             server_request('POST', '/api/v1/octo/printer_events/', self.plugin, timeout=60, raise_exception=True, files=None, data=data, headers=self.plugin.auth_headers())
+            _logger.debug('server notified celestrius is done')
         except Exception as e:
             _logger.warning('Failed to notify celestrius completed' + str(e))
