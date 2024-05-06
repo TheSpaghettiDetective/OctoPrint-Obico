a3a9ee8c1c04364551ae168e2efe5b4d0a11077a
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-05-06 09:43:54 -0700

    Bump version 2.4.12

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_obico"
 plugin_name = "OctoPrint-Obico"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "2.4.11"
+plugin_version = "2.4.12"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
