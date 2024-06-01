31137cc169e2fb2578bb274f01c4519281559692
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-06-01 05:31:07 -0700

    Remove compat streaming as they are no longer relevant

--- octoprint_obico/static/js/ObicoSettings.js
+++ octoprint_obico/static/js/ObicoSettings.js
@@ -33,7 +33,7 @@ $(function () {
         );
         self.showDetailPage = ko.observable(false);
         self.serverStatus = ko.mapping.fromJS({ is_connected: false, status_posted_to_server_ts: 0, bailed_because_tsd_plugin_running: false });
-        self.streaming = ko.mapping.fromJS({ is_pi_camera: false, webrtc_streaming: false, compat_streaming: false});
+        self.streaming = ko.mapping.fromJS({ is_pi_camera: false, webrtc_streaming: false});
         self.linkedPrinter = ko.mapping.fromJS({ is_pro: false, id: null, name: null});
         self.errorStats = ko.mapping.fromJS({ server: { attempts: 0, error_count: 0, first: null, last: null }, webcam: { attempts: 0, error_count: 0, first: null, last: null }});
         self.serverTestStatusCode = ko.observable(null);
