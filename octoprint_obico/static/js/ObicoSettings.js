92ec69a53dbe4d36047384049d7dc5981e9e42ba
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-06-01 07:14:13 -0700

    Primary camera settings page

--- octoprint_obico/static/js/ObicoSettings.js
+++ octoprint_obico/static/js/ObicoSettings.js
@@ -48,7 +48,18 @@ $(function () {
         self.disableWizardAutoPopUp = ko.observable(false);
         self.serverType = ko.observable('cloud');
         self.hasTsdMigratedModalShown = ko.observable(false);
+        self.primaryWebcamStream = ko.pureComputed(function() {
+            var primaryStream = ko.utils.arrayFirst(self.settingsViewModel.settings.plugins.obico.webcam_streams(), function(stream) {
+                return stream.is_primary_camera && stream.is_primary_camera() === true;
+            });
 
+            if (!primaryStream) {
+                primaryStream = { is_primary_camera: ko.observable(true), name: ko.observable('classic') };
+                self.settingsViewModel.settings.plugins.obico.webcam_streams.push(primaryStream);
+            }
+
+            return primaryStream;
+        });
         self.onStartupComplete = function (plugin, data) {
             self.fetchPluginStatus();
             self.serverType(getServerType(self.settingsViewModel.settings.plugins.obico.endpoint_prefix()));
